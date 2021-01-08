15cd9c15341633fd6560ff0491ae04aef6d61297
brabii <collectorsgarbages@gmail.com>
2021-01-08 17:36:55 +0100

    initialiser projet. closes #1

---
+++ src/main/java/com/beer/order/MdBeerOrderApplication.java
@@ -0,0 +1,13 @@
+package com.beer.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdBeerOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdBeerOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beer/order/MdBeerOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdBeerOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
